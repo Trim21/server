51ef099b04f69ec7196567f7d001b8d1abe94f4b
Trim21 <trim21.me@gmail.com>
2022-01-16 06:10:02 +0800

    remove unused code

--- pol/api/v0/depends/__init__.py
+++ pol/api/v0/depends/__init__.py
@@ -1,12 +1,9 @@
 from fastapi import Path, Depends
 
 from pol import res
-from pol.config import CACHE_KEY_PREFIX
 from pol.models import PublicUser
-from pol.depends import get_redis
 from pol.permission import Role
 from pol.models.subject import Subject
-from pol.redis.json_cache import JSONRedis
 from pol.api.v0.depends.auth import optional_user
 from pol.services.user_service import UserService
 from pol.services.subject_service import SubjectService
@@ -27,29 +24,6 @@ async def get_public_user(
         raise not_found
 
 
-async def _get_subject(
-    not_found: res.HTTPException = Depends(res.not_found_exception),
-    subject_id: int = Path(..., gt=0),
-    service: SubjectService = Depends(SubjectService.new),
-    redis: JSONRedis = Depends(get_redis),
-) -> Subject:
-    """get a basic subject without any check"""
-    cache_key = CACHE_KEY_PREFIX + f"subject:{subject_id}"
-    subject = await redis.get_with_model(cache_key, Subject)
-    if not subject:
-        try:
-            subject = await service.get_by_id(subject_id)
-        except SubjectService.NotFoundError:
-            raise not_found
-        await redis.set_json(cache_key, value=subject.dict(), ex=300)
-
-    if not subject:
-        raise Exception
-
-    # don't know why mypy can't narrow this type to non-optional instance
-    return subject  # type: ignore
-
-
 async def get_subject(
     subject_id: int = Path(..., gt=0),
     subject_service: SubjectService = Depends(SubjectService.new),
@@ -66,14 +40,3 @@ async def get_subject(
         )
     except SubjectService.NotFoundError:
         raise not_found
-
-
-async def get_readable_subject(
-    subject: Subject = Depends(_get_subject),
-    not_found: res.HTTPException = Depends(res.not_found_exception),
-    user: Role = Depends(optional_user),
-) -> Subject:
-    """make sure subject visible for user"""
-    if subject.nsfw and not user.allow_nsfw():
-        raise not_found
-    return subject
