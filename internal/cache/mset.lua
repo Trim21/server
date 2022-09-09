ed810572f7d3cbb2b73166cf0a83b49a07f8b800
Trim21 <trim21.me@gmail.com>
2022-09-10 07:42:17 +0800

    add comment to lua

--- internal/cache/mset.lua
+++ internal/cache/mset.lua
@@ -1,3 +1,6 @@
+-- keys [     k1, k2, k3]
+-- args [ttl, v1, v2, v3]
+
 ---@type integer
 local keyLen = #KEYS
 
@@ -7,6 +10,5 @@ for i = 1, keyLen do
     local value = redis.call('set', KEYS[i], ARGV[i + 1], 'ex', ttl)
     -- do some logic on the value
 end
-
 
 return keyLen
